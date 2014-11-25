82a181305c09aeeb1550a6fd7d32117e9e5eef46
Fabian Zimmer <fabian@qfma.de>
2014-11-25 22:59:16 +0000

    Added a simple doorbell button handler

--- doorbell.py
+++ doorbell.py
@@ -1,3 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import RPi.GPIO as GPIO
+import time
+
+from pibell.sim900 import Sim900
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+def call():
+    number = ""
+    sim900 = Sim900()
+    sim900.call(number)
+
+
+while True:
+    input_state = GPIO.input(21)
+    if input_state == False:
+        print('Putton bressed!')
+        call()
+        time.sleep(0.2)
